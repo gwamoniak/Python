af4adf09b20fcbb367d02af6516a615f117275c3
Lukasz <pictureofme@outlook.com>
2022-03-29 16:25:11 +0200

    is prime

---
+++ others/isprime.py
@@ -0,0 +1,16 @@
+import math 
+
+def isPrime(x)
+
+    n = abs(int(x))
+    
+    if n < 2:
+        return False
+    elif n == 2
+        return True
+    for element in range(2,n):
+        if element%2 == 0:
+            return False
+    return True     
+        
+    
