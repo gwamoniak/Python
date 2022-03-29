a93405799f29c2930ff18ae63c8488ca40f3a78e
Lukasz <pictureofme@outlook.com>
2022-03-29 17:03:28 +0200

    Update isprime.py

--- others/isprime.py
+++ others/isprime.py
@@ -6,10 +6,10 @@ def isPrime(x)
     
     if n < 2:
         return False
-    elif n == 2
+    elif n == 2:
         return True
     for element in range(2,n):
-        if element%2 == 0:
+        if n%element == 0:
             return False
     return True     
         
